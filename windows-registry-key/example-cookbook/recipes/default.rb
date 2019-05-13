ce260b2cdcdd78bf4751242b52defb163ec81b26
Gregory K. Spranger <gkspranger@Gregorys-MacBook-Pro.local>
2019-05-13 08:57:46 -0400

    try again

--- windows-registry-key/example-cookbook/recipes/default.rb
+++ windows-registry-key/example-cookbook/recipes/default.rb
@@ -1,9 +1,9 @@
 Chef::Log.warn 'hello world'
 
-# registry_key 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\Terminal Services' do
-#   values [
-#     { name: 'DisablePasswordSaving',
-#       type: :string,
-#       data: '1' }
-#   ]
-# end
+registry_key 'HKEY_LOCAL_MACHINE\\SOFTWARE\\Policies\\Microsoft\\Windows NT\\Terminal Services' do
+  values [
+    { name: 'DisablePasswordSaving',
+      type: :string,
+      data: '1' }
+  ]
+end
