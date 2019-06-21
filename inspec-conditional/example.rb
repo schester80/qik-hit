ca8f15aa31a0a000b29930a6b3477e3c944f957a
Gregory K. Spranger <gkspranger@Gregorys-MacBook-Pro.local>
2019-06-21 16:50:31 -0400

    fun

--- inspec-conditional/example.rb
+++ inspec-conditional/example.rb
@@ -12,3 +12,32 @@ else
     it { should_not be_installed }
   end
 end
+
+if service('sshd').installed?
+  describe service 'sshd' do
+    it { should be_running }
+    it { should be_enabled }
+  end
+end
+
+control 'example service' do
+  impact 0.2
+
+  if service('example1').installed?
+    myapp = 'example1'
+  elsif service('example2').installed?
+    myapp = 'example2'
+  elsif service('example3').installed?
+    myapp = 'example3'
+  end
+
+  title "Ensure that #{myapp} service is running"
+  desc "#{myapp} service needs to be running for app availablity"
+
+  if service(myapp).installed?
+    describe service(myapp) do
+      it { should be_enabled }
+      it { should be_running }
+    end
+  end
+end
